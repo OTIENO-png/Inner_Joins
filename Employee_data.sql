3cdb0a5a8b7964585f8bcdf8b0f66bd20eeeee73
brian <otienolubalo@gmail.com>
2025-03-26 12:10:48 +0300

    Keyed in dtat about the employee into the employee table

---
+++ Employee_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Employee_Data(Employee_id_No, Full_Name, Email, Birth_year)
+VALUES
+("2850","Kenny Allan","kennyallan@gamil.com","1971"),
+("2961","Ann Weta","annweta@gamil.com","1981"),
+("2800","incent Kilimo","vincentkilimo@gamil.com","1969"),
+("2706","Ruto Kamau","rutokamau@gamil.com","1988"),
+("2808","Millicent Yetu","millicentyetu@gamil.com","2000"),
+("3109","Mkenya Africa","mkenyarica@gamil.com","2003");
+
+SELECT *FROM Employee_Data;
